6424aad7e54db841091b1545e2856ca1ccd1afa5
Geoffrey Gao <107231152+geoffreygao1@users.noreply.github.com>
2023-01-04 12:35:32 +0900

    Initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+// Placeholder test script
+
+// Example:
+// describe('Triangle', () => {
+//   test('should correctly determine whether three lengths make an isosceles triangle', () => {
+//     const isoscTriangle = new Triangle(5,5,7)
+//     expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+//   });
+// });
---
+++ src/index.js
@@ -0,0 +1,5 @@
+//Placeholder script
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
