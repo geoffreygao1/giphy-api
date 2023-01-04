e6281f24b95a5a9dda5533f9d4cf8198ec109979
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-04 12:51:47 +0900

    INitial js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,43 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+// Business Logic
+
+function getGifs(keyword) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&limit=3&apikey=${process.env.API_KEY}`
+
+  request.addEventListener("loadend", function () {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, keyword);
+    } else {
+      printError(this, response, keyword);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+// UI Logic
+
+function printError(request, apiResponse, keyword) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+function printElements(apiResponse, keyword) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const keyword = document.querySelector('#keyword').value;
+  document.querySelector('#keyword').value = null;
+  getGifs(keyword);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
